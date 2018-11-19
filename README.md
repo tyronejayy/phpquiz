476850b61bb0fc764e7042eab6e18588d4031baa
tyronejayy <tyronejoubert22@gmail.com>
2018-11-19 16:29:23 +0200

    styling and content added

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# phpquiz
-php quiz project
+#PHP-QUIZ
+
+Technologies and Tools Used:
+
+Languages:
+
+- CSS
+- HTML
+- PHP
+
+Tools:
+
+- VS Code
+- GitBash
+
+Changelog
+2018-11-19:
+
+Content added:
+Content Added
+Styling Done
+Some question inserted
+Readme Updated
+
+Contributors
+Tyrone joubert - 2018
---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>phpquiz</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+</head>
+<body>
+
+
+    <h1>Quiz-onator 3000</h1> <br> <br>
+    <section class="container">
+        <form class="quiz" action=".php" method="post" enctype="multipart/form-data">
+            <div class="questions">1] Who let the dogs out?</div>
+            <p><input type="radio" name="answerOne"> Who, who, who, who</p>
+            <p><input type="radio" name="answerOne"> Woof, woof, woof, woof</p>
+            <p><input type="radio" name="answerOne"> The gatekeeper</p>
+            <p><input type="radio" name="answerOne"> The ghettos</p>
+
+            <br>
+            <br>
+
+            <div class="questions">2] What was the weather like on the 3rd August 2018?</div>
+            <p><input type="radio" name="answerTwo"> Partly Cloudy</p>
+            <p><input type="radio" name="answerTwo">Sunny</p>
+            <p><input type="radio" name="answerTwo">60%rain</p>
+            <p><input type="radio" name="answerTwo">30%rain</p>
+
+            <br>
+            <br>
+
+            <div class="questions">3] Choose the oldest game?</div>
+            <p><input type="radio" name="answerThree">Snake on Nokia 3310</p>
+            <p><input type="radio" name="answerThree">Mario Bros</p>
+            <p><input type="radio" name="answerThree">Tetris</p>
+            <p><input type="radio" name="answerThree">Contra</p>
+
+            <br>
+            <br>
+
+            <div class="questions">4] Where was the fortune cookie actually invented?</div>
+            <p><input type="radio" name="answerFour">England</p>
+            <p><input type="radio" name="answerFour">Japan</p>
+            <p><input type="radio" name="answerFour">China</p>
+            <p><input type="radio" name="answerFour">America</p>
+
+            <br>
+            <br>
+
+            <div class="questions">5] What is a group of crows called?</div>
+            <p><input type="radio" name="answerFive"> A treasure</p>
+            <p><input type="radio" name="answerFive"> A murder</p>
+            <p><input type="radio" name="answerFive"> A bundle</p>
+            <p><input type="radio" name="answerFive"> A flock</p>
+
+            <br>
+            <br>
+
+            <div class="questions">6] How long is a kangaroo baby when it s born?</div>
+            <p><input type="radio" name="answerSix">500cm</p>
+            <p><input type="radio" name="answerSix">20inches</p>
+            <p><input type="radio" name="answerSix">1inch</p>
+            <p><input type="radio" name="answerSix">about a human baby's size</p>
+
+            <br>
+            <br>
+
+            <div class="questions">7] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">8] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">9] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">10] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">11] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">12] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">13] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">14] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">15] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">16] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">17] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">18] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">19] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <div class="questions">20] ?</div>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+            <p><input type="radio" name="answerOne"></p>
+
+            <br>
+            <br>
+
+            <input type="submit">
+        </form>
+    </section>
+</body>
+</html>
